df206e788564380b5acf817cbd33b1fdec67e714
ajzia <jokul502@wp.pl>
2022-03-23 01:45:24 +0100

    Nearest neighbour algorithm added.

--- main.jl
+++ main.jl
@@ -22,11 +22,11 @@ end
   Returns path's weight.
 
 ## Parameters:
-- `path::Vector{T<:Integer}`: path with given order of visited nodes
-- `weights::AbstractMatrix{Float64}`: matrix of weights between nodes
+- `path::Vector{T<:Integer}`: path with a given order of visited nodes.
+- `weights::AbstractMatrix{Float64}`: matrix of weights between nodes.
 
 ## Returns:
-- Sum of weights between nodes in the path
+- Sum of weights between nodes in the path.
 
 """
 function calculate_path(path::Vector{T}, weights::AbstractMatrix{Float64}) where T<:Integer
@@ -47,7 +47,7 @@ end
 - `k::{Int}`: the number of repetitions.
 
 ## Returns:
-- The best path and its length found from `k` repetitions
+- The best path and its length found from `k` repetitions.
 
 """
 function k_random(tsp_dict::Dict, k::Int)
@@ -59,22 +59,106 @@ function k_random(tsp_dict::Dict, k::Int)
     path = shuffle(collect(1:tsp_dict[:dimension]))
     length = calculate_path(path, tsp_dict[:weights])
     
-    best_path, min_length = (i == 1 || min_length > length) ?  (path, length) : (best_path, min_length)
+    best_path, min_length = (i == 1 || min_length > length) ? (path, length) : (best_path, min_length)
   end
   return (best_path, min_length)
 end
 
 """
-  Main program function
+    nearest_neighbour(node, dimension, weights) -> (Array{Float64}, Float64)
+  Calculates the best path using nearest neighbour algorithm with a given starting node.
+
+### Parameters:
+- `node::Int`: starting node.
+- `dimension::Int`: number of nodes in a path.
+- `weights::AbstractMatrix{Float64}`: matrix of weights between nodes.
+
+### Returns:
+- `Array{Int64}`: the best path found.
+- `Float64`: path's weight.
+
+"""
+function nearest_neighbour(node::Int, dimension::Int, weights::AbstractMatrix{Float64})
+  @assert node <= dimension && node > 0
+  
+  non_visited = collect(1:dimension)
+  deleteat!(non_visited, node)
+  
+  path = [node]
+  
+  for i in 1:dimension-1
+    min_length = maximum!(float([1]), weights[path[end], :])[1] + 1
+    nearest_node = Int64
+
+    for i in 1:size(non_visited, 1)
+      len = weights[path[end], non_visited[i]]
+    
+      (nearest_node, min_length) = min_length > len ? (non_visited[i], len) : (nearest_node, min_length)
+    end
+    
+    push!(path, nearest_node)
+    deleteat!(non_visited, findall(x -> x == nearest_node, non_visited))
+  end
+
+  return (path, calculate_path(path, weights))
+end
+
+"""
+    repetitive_nearest_neighbour(tsp_dict) -> (Array{Float64}, Float64)
+  Returns the best path using nearest neighbour algorithm, having `i = 1,2,...,n` as a starting node.
+
+## Parameters:
+- `dimension::Int`: number of nodes in a path.
+- `weights::AbstractMatrix{Float64}`: matrix of weights between nodes.
+
+## Returns:
+- `Array{Int64}`: the best path found.
+- `Float64`: path's weight.
+
+"""
+function repetitive_nearest_neighbour(tsp_dict::Dict)
+  best_path = Vector{Int64}
+  min_length = Float64
+
+  for i in 1:tsp_dict[:dimension]
+    path, length = nearest_neighbour(i, tsp_dict[:dimension], tsp_dict[:weights])
+
+    best_path, min_length = (i == 1 || min_length > length) ? (path, length) : (best_path, min_length)
+  end
+
+  return (best_path, min_length)
+end
+
+"""
+    tsp_test(name, f, tsp_dict)
+  Runs test for a given algorithm with `tsp_dict`.
+  Requires function `func` to determine the best path for the algorithm.
+
+## Params:
+- `name::{String}`: name of the algorithm.
+- `func::{Function}`: function used to calculate a path.
+- `tsp_dict::Dict`: `TSP` dataset.
+
+"""
+function tsp_test(name::String, func::Function, tsp_dict::Dict, args...)
+  (path, weight) = func(tsp_dict, args...)
+  println("Algorithm: $name")
+  println("Path: $path \nDistance: $weight\n")
+end
+
+"""
+  Main program function.
 """
 function main()
   # reading .tsp file and converting it to dictionary
-  tsp = readTSP("./all/gr17.tsp")
+  tsp = readTSP("./all/eil51.tsp")
   tsp_dict = struct_to_dict(tsp)
 
   # k_random test
-  path, weight = k_random(tsp_dict, 100000)
-  println("\nDistance: ", weight, "  Path: ", path)
+  tsp_test("k-random", k_random, tsp_dict, 100000)
+
+  # repetitive_nearest_neighbour test
+  tsp_test("repetitive nearest neighbour", repetitive_nearest_neighbour, tsp_dict)
 end
 
 main()
